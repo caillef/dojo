d3ef2d87bb886b5d9cc4d6b531eef44a543f7f54
Corentin Cailleaud <corentin.cailleaud@epitech.eu>
2024-07-26 10:58:51 +0200

    fix: update wasm dependencies

--- crates/torii/client/src/lib.rs
+++ crates/torii/client/src/lib.rs
@@ -1,7 +1,7 @@
 #[cfg(target_arch = "wasm32")]
 extern crate wasm_prost as prost;
 #[cfg(target_arch = "wasm32")]
-extern crate wasm_tonic as tonic;
+extern crate tonic;
 
 pub mod client;
 pub mod utils;
